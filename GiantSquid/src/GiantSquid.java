0369de3414ae3a69db0fa071e693173fb0f17c29
laarnib <edulaarni@gmail.com>
2021-12-06 16:17:51 -0500

    add part 2 solution.  Code is messy and will refactor later

--- GiantSquid/src/GiantSquid.java
+++ GiantSquid/src/GiantSquid.java
@@ -4,28 +4,35 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Set;
 
-//Set<Integer> setOfKeySet = bingoCard.keySet();
-
-        /* Looping through hashmap
-        for(Integer key : setOfKeySet) {
-            System.out.println("\n Key #" + key);
-
-            for (ArrayList<String> nums : bingoCard.get(key)) {
-                System.out.println("\t\t" + nums);
-            }
-        } */
-
 public class GiantSquid {
 
     public static void main(String[] args) throws Exception {
         Scanner puzzleInput = new Scanner(Paths.get("input.txt"));
         ArrayList<String> drawnNumbers = new ArrayList<>();
-        HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard = new HashMap<>();
-
-
-
+        HashMap<Integer, ArrayList<ArrayList<String>>> bingoCards = new HashMap<>();
 
         // Get the list of drawn numbers
+        drawnNumbers = getListOfDrawnNumbers(puzzleInput);
+
+        // Create a hashmap of the bingo cards
+        bingoCards = createHashMap(puzzleInput);
+
+        // Get the first Winning bingo card
+        //getFirstWinningBoard(bingoCards, drawnNumbers);
+
+        // Get the last Winning bingo card
+        getLastWinningBoard(bingoCards, drawnNumbers);
+
+        puzzleInput.close();
+    }
+
+
+
+
+    // Get the list of drawn numbers from the input file.
+    public static ArrayList<String> getListOfDrawnNumbers(Scanner puzzleInput) {
+        ArrayList<String> drawnNumbers = new ArrayList<>();
+
         if (puzzleInput.hasNextLine()) {
             String[] list = puzzleInput.nextLine().split(",");
             int size = list.length;
@@ -35,19 +42,10 @@ public class GiantSquid {
             }
         }
 
-        System.out.println("Drawn Numbers: " + drawnNumbers);
-        System.out.println("");
-
-        // Create bingo card
-        bingoCard = createHashMap(puzzleInput);
-        getFirstWinningBoard(bingoCard, drawnNumbers);
-
-        System.out.println("Bing cards:");
-        // Play bingo
-
-        puzzleInput.close();
+        return drawnNumbers;
     }
 
+    // Create a HashMap of the bingo cards
     public static HashMap<Integer, ArrayList<ArrayList<String>>> createHashMap(Scanner puzzleInput) {
         HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard = new HashMap<>();
         ArrayList<ArrayList<String>> outerArrayList = new ArrayList<>();
@@ -78,20 +76,172 @@ public class GiantSquid {
         return bingoCard;
     }
 
-    /*public static int playBingo(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard, ArrayList<String> drawnNumbers) {
-        int score = 0;
-        int drawnNumbersCount = drawnNumbers.size();
+    public static void getLastWinningBoard(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCards, ArrayList<String> drawnNumbers) {
+        //System.out.println(bingoCards);
+        // Draw first five numbers and check if there is a winner
+        String winningValue = "";
+        String drawNumber = "";
+        int winningCard = -1;
+        HashMap<Integer, ArrayList<ArrayList<String>>> copy = bingoCards;
+        Set<Integer> hashKeys = copy.keySet();
 
-        for (int i = 0; i < drawnNumbersCount; i++) {
-            for (H=)
+        //System.out.println("Draw first five numbers");
+        for (int i = 0; i < 5; i++) {
+            drawNumber = drawnNumbers.get(i);
+            //System.out.println("Draw Number: " + drawNumber);
+            for (Integer key : hashKeys) {
+                //System.out.println("\t Bingo card #" + key + ": ");
+                for (ArrayList<String> row : bingoCards.get(key)) {
+                    for (String ball : row) {
+                        if (ball.equals(drawNumber)) {
+                            int indexBall = row.indexOf(ball);
+                            row.set(indexBall, "X");
+                        }
+                    }
+                   // System.out.println("\t\t" + row);
+                }
+            }
         }
 
-        return score;
-    }*/
+        // Check for winner
+        winningCard = checkWinner(copy);
 
-    public static void getFirstWinningBoard(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard, ArrayList<String> drawnNumbers) {
+        if (winningCard != -1) {
+            //System.out.println("Winning card: " + winningCard);
+            //System.out.println("Winning value: " + winningValue);
+            //copy.remove(winningCard);
+            winningCard = -1;
+        } else {
+            ArrayList<ArrayList<String>> lastWinningCard = new ArrayList<>();
+
+            // Draw more numbers
+            int index = 5;
+            while (copy.size() > 0) {
+                int keyToPass = 0;
+                ArrayList<Integer> keysToRemove = new ArrayList<>();
+                if (index > drawnNumbers.size()) {
+                    break;
+                }
+
+                if (copy.size() == 0) {
+                    break;
+                }
+
+                drawNumber = drawnNumbers.get(index);
+                //System.out.println("Draw numbers index : " + index);
+                //System.out.println("Draw number: " + drawNumber);
+
+                for (Integer key : hashKeys) {
+                    //System.out.println("\t Bingo card #" + key + ": ");
+                    if (copy.size() == 1) {
+                        keyToPass = key;
+                    }
+                    for (ArrayList<String> row : bingoCards.get(key)) {
+                        for (String ball : row) {
+                            if (ball.equals(drawNumber)) {
+                                int indexBall = row.indexOf(ball);
+                                row.set(indexBall, "X");
+                            }
+                        }
+                        //System.out.println("\t\t" + row);
+                    }
+                }
+
+                if (copy.size() > 1) {
+                    keysToRemove = removeAllWinners(copy);
+                }
+
+                if (copy.size() == 1) {
+                    boolean isWinner = isWinningCard(copy.get(keyToPass));
+                    if(isWinner) {
+                        winningValue = drawNumber;
+                        lastWinningCard = copy.get(keyToPass);
+                        int score = getScore(lastWinningCard, winningValue);
+                        System.out.println(score);
+                        break;
+                    }
+                }
+
+                for (Integer key : keysToRemove) {
+                    copy.remove(key);
+                }
+
+                index++;
+            }
+        }
+    }
+
+    public static ArrayList<Integer> removeAllWinners(HashMap<Integer, ArrayList<ArrayList<String >>> copyBingoCard) {
+        ArrayList<Integer> keysToRemove = new ArrayList<>();
+
+        Set<Integer> keys = copyBingoCard.keySet();
+        for (Integer key : keys) {
+            boolean isWinner = isWinningCard(copyBingoCard.get(key));
+
+            if (isWinner) {
+                keysToRemove.add(key);
+            }
+        }
+        //System.out.println(keysToRemove);
+        return keysToRemove;
+    }
+
+
+    public static boolean isWinningCard(ArrayList<ArrayList<String>> card) {
+        boolean isWinner = isWinningHorizontal(card);
+
+        if (!isWinner) {
+            isWinner = isWinningVertical(card);
+        }
+
+        return isWinner;
+    }
+
+    public static boolean isWinningHorizontal(ArrayList<ArrayList<String>> card) {
+        int count = 0;
+        int size = card.get(0).size();
+        //System.out.println(card);
+        for (ArrayList<String> row : card) {
+            for (String ball : row) {
+                if (ball.equals("X")) {
+                    count++;
+                }
+
+                if (count == size) {
+                    return true;
+
+                }
+            }
+            count = 0;
+        }
+
+        return false;
+    }
+
+    public static boolean isWinningVertical(ArrayList<ArrayList<String>> card) {
+        int count = 0;
+        int size = card.get(0).size();
+        int foundAtColumn = -1;
+        int column = 0;
+        while(column < size ) {
+            for (int j = 0; j < size; j++) {
+                if(card.get(j).get(column).equals("X")) {
+                    count++;
+                }
+            }
+
+            if (count == size) {
+                return true;
+            }
+
+            count = 0;
+            column++;
+        }
+        return false;
+    }
+    public static void getFirstWinningBoard(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCards, ArrayList<String> drawnNumbers) {
+        HashMap<Integer, ArrayList<ArrayList<String>>> copy = bingoCards;
         int hasWinner = -1;
-        // boolean hasWinner = false;
         String value = "";
         int score = 0;
         int index = 0;
@@ -101,10 +251,10 @@ public class GiantSquid {
                 value = drawnNumbers.get(index);
                 System.out.println("Draw Number: " + value);
             }
-            Set<Integer> setOfKeySet = bingoCard.keySet();
+            Set<Integer> setOfKeySet = copy.keySet();
             for(Integer key : setOfKeySet) {
                 System.out.println("\t Bingo card #" + key + ": ");
-                for (ArrayList<String> nums : bingoCard.get(key)) {
+                for (ArrayList<String> nums : copy.get(key)) {
                     for (String ball : nums) {
                         if (ball.equals(value)) {
                             int indexBall = nums.indexOf(ball);
@@ -116,7 +266,7 @@ public class GiantSquid {
 
             }
 
-            hasWinner = checkWinner(bingoCard);
+            hasWinner = checkWinner(copy);
 
             if (hasWinner > -1) {
                 System.out.println("Inside hasWinner");
@@ -127,7 +277,7 @@ public class GiantSquid {
             index++;
         }
         System.out.println(value);
-        score = getSum(bingoCard.get(hasWinner)) * Integer.valueOf(value);
+        score = getScore(copy.get(hasWinner), value);
         System.out.println(score);
     }
     public static int checkWinner(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard) {
@@ -175,11 +325,12 @@ public class GiantSquid {
         int winner = -1;
         int count = 0;
 
-        int rowSize = bingoCard.get(1).size();
-        int columnSize = bingoCard.get(1).get(0).size();
+
 
 
         for(Integer key : setOfKeySet) {
+            int rowSize = bingoCard.get(key).size();
+            int columnSize = bingoCard.get(key).get(0).size();
             int foundXAtColumn = -1;
             // Find X at first row of bingo card, if X is found
             // store the index of the column.  If no X is found
@@ -221,7 +372,7 @@ public class GiantSquid {
         return winner;
     }
 
-    public static int getSum(ArrayList<ArrayList<String>> boards) {
+    public static int getScore(ArrayList<ArrayList<String>> boards, String value) {
         int sum = 0;
 
         for (ArrayList<String> board : boards) {
@@ -231,6 +382,6 @@ public class GiantSquid {
                 }
             }
         }
-        return sum;
+        return sum * Integer.valueOf(value);
     }
 }
