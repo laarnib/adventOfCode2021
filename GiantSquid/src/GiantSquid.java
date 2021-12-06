12ef3543aeced3a265facc288e22f89a14e33a2c
laarnib <edulaarni@gmail.com>
2021-12-05 22:39:42 -0500

    - modify solution

--- GiantSquid/src/GiantSquid.java
+++ GiantSquid/src/GiantSquid.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class GiantSquid {
 
     public static void main(String[] args) throws Exception {
-        Scanner puzzleInput = new Scanner(Paths.get("sample2.txt"));
+        Scanner puzzleInput = new Scanner(Paths.get("input.txt"));
         ArrayList<String> drawnNumbers = new ArrayList<>();
         HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard = new HashMap<>();
 
@@ -38,18 +38,24 @@ public class GiantSquid {
             }
         }
 
+        System.out.println("Drawn Numbers: " + drawnNumbers);
+        System.out.println("");
+
         // Create bingo card
         bingoCard = createHashMap(puzzleInput);
         int index = 0;
 
+        System.out.println("Bing cards:");
         // Play bingo
         while (hasWinner == -1) {
-
+            System.out.println("Inside while: " + value);
             if (index < drawnNumbers.size()) {
                value = drawnNumbers.get(index);
+                System.out.println("Draw Number: " + value);
             }
             Set<Integer> setOfKeySet = bingoCard.keySet();
             for(Integer key : setOfKeySet) {
+                System.out.println("\t Bingo card #" + key + ": ");
                 for (ArrayList<String> nums : bingoCard.get(key)) {
                     for (String ball : nums) {
                         if (ball.equals(value)) {
@@ -57,28 +63,22 @@ public class GiantSquid {
                             nums.set(indexBall, "X");
                         }
                     }
+                    System.out.println("\t\t" + nums);
                 }
+
             }
 
-            hasWinner = checkVerticalWinner(bingoCard);
+            hasWinner = checkWinner(bingoCard);
 
             if (hasWinner > -1) {
+                System.out.println("Inside hasWinner");
+                System.out.println("Winning key: " + hasWinner);
                 break;
-            } else {
-                hasWinner = checkWinnerHorizontal(bingoCard);
-
-                if (hasWinner > -1) {
-                    break;
-                }
             }
 
             index++;
         }
-
         System.out.println(value);
-        System.out.println(hasWinner);
-        System.out.println(bingoCard);
-
         score = getSum(bingoCard.get(hasWinner)) * Integer.valueOf(value);
         System.out.println(score);
         puzzleInput.close();
@@ -125,7 +125,19 @@ public class GiantSquid {
         return score;
     }*/
 
-    public static int checkVerticalWinner(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard) {
+    public static int checkWinner(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard) {
+        int winner = checkWinnerHorizontal(bingoCard);
+
+       if (winner == -1) {
+            winner = checkWinnerVertical(bingoCard);
+        } else {
+            return winner;
+        }
+
+        return winner;
+    }
+
+    public static int checkWinnerHorizontal(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard) {
         Set<Integer> setOfKeySet = bingoCard.keySet();
 
         int count = 0;
@@ -153,49 +165,55 @@ public class GiantSquid {
         return winner;
     }
 
-    public static int checkWinnerHorizontal(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard) {
+    public static int checkWinnerVertical(HashMap<Integer, ArrayList<ArrayList<String>>> bingoCard) {
         Set<Integer> setOfKeySet = bingoCard.keySet();
-
-        int count = 0;
         int winner = -1;
+        int count = 0;
 
+        int rowSize = bingoCard.get(1).size();
+        int columnSize = bingoCard.get(1).get(0).size();
 
 
         for(Integer key : setOfKeySet) {
-            int row = 0;
-            int column = 0;
-            int rowSize = bingoCard.get(key).get(row).size();
-            for (String num : bingoCard.get(key).get(row)) {
-                if (num.equals("X")) {
-                        break;
-                }
-                column++;
-            }
-            row++;
-            count++;
-            column--;
-            // check the rest of the column
-            int totalRows = bingoCard.get(key).size();
-            while (row < totalRows){
-                System.out.println("row :" + row);
-                if (bingoCard.get(key).get(row).get(column).equals("X")) {
+            int foundXAtColumn = -1;
+            // Find X at first row of bingo card, if X is found
+            // store the index of the column.  If no X is found
+            // return winner = -1;
+            for (int i = 0; i < columnSize; i++ ) {
+                if (bingoCard.get(key).get(0).get(i).equals("X")) {
+                    foundXAtColumn = i;
                     count++;
-                    row++;
-                } else {
                     break;
                 }
             }
+            //System.out.println("Checking Vertical: ");
+            //System.out.println("\t\t" + count);
+            if (foundXAtColumn > -1) {
+                // Check remaining rows and if the column where X is found
+                // matches the first row, increment count.  When count is
+                // equal to five, winner = key of hashmap where the winner
+                // is found
+                for (int row = 1; row < rowSize; row++) {
+                    for (int col = 0; col < columnSize; col++) {
+                        if (bingoCard.get(key).get(row).get(col).equals("X") && col == foundXAtColumn) {
+                            count++;
+                            //System.out.println("\t\t" + count);
+                        }
+                    }
+                }
+            } else {
+                continue;
+            }
 
-            if (count == totalRows) {
+            //System.out.println("Total count: " + count);
+            if (count == rowSize) {
                 winner = key;
-                return winner;
             } else {
                 count = 0;
             }
         }
 
         return winner;
-
     }
 
     public static int getSum(ArrayList<ArrayList<String>> boards) {
